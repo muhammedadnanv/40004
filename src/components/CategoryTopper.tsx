c02f892b388866e6572b728f78c1918eb1a394e2
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-12-27 19:48:17 +0530

    Update CategoryTopper.tsx

--- src/components/CategoryTopper.tsx
+++ src/components/CategoryTopper.tsx
@@ -8,7 +8,8 @@ export const CategoryTopper = () => {
       <AlertTitle>Important Notice</AlertTitle>
       <AlertDescription className="text-sm">
         We exclusively support and verify mentors in Men's and Women's categories. All other categories are considered potential spam and are not endorsed by our platform.<br />
-          Make sure you pay the amount quickly; otherwise, the payment will fail, and you'll have to wait 7 days to get the amount credited back to your account.
+          Make sure you pay the amount quickly; otherwise, the payment will fail, and you'll have to wait 7 days to get the amount credited back to your account.<br />
+        Please ensure that you have made the payment through your UPI ID or number. Do not scan the QR code
       </AlertDescription>
     </Alert>
   );
